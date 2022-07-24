2de3ae6c64ac8b2b340316a0cf72ce31a900e6aa
Github-MitchD <hello@michel-dufour.fr>
2022-07-24 18:47:24 +0200

    new version

--- script.js
+++ script.js
@@ -2,47 +2,58 @@
 //  _CODE JS BY MITCH.D
 //  _Developpeur Web 2021
 /*--------------------------------------------------------*/
-let nouveau   = document.querySelector('#nouveau');
-let citation  = document.querySelector('#citation');
-let auteur    = document.querySelector('#auteur');
 
-let dernier   = 0;
-let nombreAleatoire = 0;
+// Tableau de citations
 let citations = [
-  ["La vie est un mystère qu'il faut vivre, et non un problème à résoudre.", "Gandhi"],
-  ["Le plus grand risque est de ne prendre aucun risque.", "Mark Zuckerberg"],
-  ["Méritez votre statut de leader chaque jour.", "Mickael Jordan"],
-  ["Soyez le changement que vous voulez voir dans le monde.", "Gandhi"],
-  ["A chaque fois que vous vous retrouvez du même côté que la majorité, il est temps de prendre du recul, et de réfléchir.", "Mark Twain"],
-  ["Seulement ceux qui prendront le risque d’aller trop loin découvriront jusqu’où on peut aller.", "T.S Elliot"],
-  ["Le succès c’est tomber sept fois, se relever huit.", "Proverbe japonais"],
-  ["Dans vingt ans vous serez plus déçus par les choses que vous n’avez pas faites que par celles que vous avez faites. Alors sortez des sentiers battus. Mettez les voiles. Explorez. Rêvez. Découvrez.", "Mark Twain"],
-  ["Si vous attendez pour agir, tout ce que vous gagnerez, avec le temps, c’est de l’âge.", "Brian Tracy"],
-  ["Quand on concentre son attention sur un seul projet, l’esprit suggère constamment des idées et des améliorations qui lui échapperaient s’il était occupé avec plusieurs projets en même temps.", "P.T. Barnum"],
-  ["Se dédier à faire tout ce que l’on peut pour aider les autres à obtenir ce qu’ils veulent, c’est la clé du succès.", "Brian Sher"],
-  ["Si vous pensez que vous êtes trop petit pour avoir de l’impact, essayez d’aller au lit avec un moustique.", "Anita Roddick"],
-  ["Ne jugez pas chaque jour sur ce que vous récoltez, mais sur les graines que vous semez.", "Robert Louis Stevenson"],
-  ["L’action est la clé fondamentale de tout succès.", "Pablo Picasso"],
-  ["Le succès, c’est se promener d’échecs en échecs tout en restant motivé.", "Winston Churchill"],
-  ["Votre avenir est créé par ce que vous faîtes aujourd’hui, pas demain.", "Robert T. Kiyosaki"],
-  ["Ne vous découragez pas, c’est souvent la dernière clef du trousseau qui ouvre la porte.", "Zig Ziglar"],
-  ["Pour gagner votre vie, apprenez à l’école. Pour gagner une fortune, apprenez par vous-même.", "Brian Tracy"],
-  ["Les gagnants trouvent des moyens, les perdants des excuses…", "F. D. Roosevelt"],
-  ["Vous n’êtes jamais trop vieux pour vous fixer de nouveaux buts, ou rendre vos rêves réalité.", "C.S. Lewis"],
-  ["Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.", "Winston Churchill"]
+  [" La seule façon de faire du bon travail est d’aimer ce que vous faites. Si vous n’avez pas encore trouvé, continuez à chercher. ", "Steve Jobs"],
+  [" Il n’y a qu’une façon d’échouer c’est d’abandonner avant d’avoir réussi. ", "Georges Clémenceau"],
+  [" Le succès, c'est se promener d'échec en échec tout en restant motivé. ", "Winston Churchill "],
+  [" Ceux qui abandonnent ne gagnent jamais, ceux qui gagnent n’abandonnent jamais. ", "Napoleon Hill"],
+  [" N’abandonnez jamais un rêve sur la base du temps qu’il faudra pour le réaliser. Le temps passera de toute façon. ", "Earl Nightingale"],
+  [" Vis comme si tu devais mourir demain, apprends comme si tu devais vivre toujours. ", "Gandhi"],
+  [" Le succès n'est pas final, l'échec n'est pas fatal. C'est le courage de continuer qui compte. ", "Winston Churchill"],
+  [" Tout est possible à qui rêve, ose, travaille et n'abandonne jamais. ", "Xavier Dolan"],
+  [" La seule limite à notre épanouissement de demain sera nos doutes d'aujourd'hui. ", "Franklin Delano Roosevelt"],
+  [" Ceux qui ne font rien ne se trompent jamais. ", "Théodore de Banville"],
+  [" Je ne perds jamais. Soit je gagne, soit j'apprends. ", "Nelson Mandela"],
+  [" Un jour, tu te réveilleras et tu n'auras plus le temps de faire ce que tu voulais faire. Fais-le donc maintenant. ", "Paulo Coelho"],
+  [" Prends le temps de réfléchir, mais lorsque c'est le moment de passer à l'action, cesse de penser et vas-y. ", "Andrew Jackson"],
+  [" Croyez en vos rêves et ils se réaliseront peut-être. Croyez en vous et ils se réaliseront sûrement. ", "Martin Luther King"],
+  [" Tu ne sais jamais à quel point tu es fort, jusqu'au jour où être fort reste ta seule option. ", "Bob Marley"],
+  [" Soyez vous-même, tous les autres sont déjà pris. ", "Oscar Wilde"],
+  [" Le but de la vie, ce n'est pas l'espoir de devenir parfait, c'est la volonté d'être toujours meilleur. ", "Ralph Waldo Emerson"],
+  [" Il n'y a qu'une façon d'échouer, c'est d'abandonner avant d'avoir réussi. ", "Georges Clemenceau"],
+  [" Ce n'est pas grave si vous avancez lentement, du moment que vous ne vous arrêtez pas. ", "Confucius"],
+  [" Vous n’êtes jamais trop vieux pour vous fixer de nouveaux buts, ou rendre vos rêves réalité. ", "C.S. Lewis"],
+  [" La gloire n'est pas de ne jamais tomber, mais de se relever chaque fois que l'on tombe. ", "Confucius"]
 ];
 
-// Fonction permettant de générer un nombre aléatoire
+let taille = citations.length;
+let last = 0;
+let nombreAleatoire = 0;
+
+let citation = document.getElementById('citation');
+let author = document.getElementById('autor');
+let btn = document.getElementById('btnNewCitation');
+
+// fonction qui genere un entier entre 0 et max
 function genererNombreEntier(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-nouveau.addEventListener('click', () => {
+// listener au click qui declenche fonction anonyme + modif texte
+btn.addEventListener('click', () => {
   do {
-    nombreAleatoire = genererNombreEntier(citations.length);
-  } while (nombreAleatoire == dernier)
-    
+    nombreAleatoire = genererNombreEntier(taille);
+  } while (last == nombreAleatoire);
+
+  // on affiche l'element a l'indice 0 de la ligne correspondant à la ligne nombreAleatoire
   citation.textContent = citations[nombreAleatoire][0];
-  auteur.textContent   = citations[nombreAleatoire][1];
-  dernier              = nombreAleatoire;
+  // on affiche l'element a l'indice 1 de la ligne correspondant à la ligne nombreAleatoire
+  author.textContent = citations[nombreAleatoire][1];
+
+  last = nombreAleatoire;
+
+  // console.log(nombreAleatoire);
+  // console.log(last);
 });
